017dfa5a96b245c7203bd36eaa4a69d9b0ec67da
Atharv Attri <atharv260107@gmail.com>
2021-08-07 15:00:32 -0700

    debug

--- scan.py
+++ scan.py
@@ -6,4 +6,5 @@ def decode_barcode(image_path: str) -> str:
     try:
         return decode(Image.open(image_path))[0].data.decode('utf-8')
     except IndexError:
+        print("AHHHHH")
         return "0"
